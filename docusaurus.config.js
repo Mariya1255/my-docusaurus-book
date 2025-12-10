3e7085c8665f7f8a3ff8f2aa7d4f047b877323aa
Mariya1255 <162633244+Mariya1255@users.noreply.github.com>
2025-12-11 03:37:56 +0500

    'docusaurus.config,js'

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,12 +23,12 @@ const config = {
   url: 'https://Mariya1255.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/humanoid-robotics-book/',
+  baseUrl: '/my-docusaurus-book/',
   
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'Mariya1255', // Usually your GitHub org/user name.
-  projectName: 'humanoid-robotics-book', // Usually your repo name.
+  projectName: '/humanoid-robotics-book/', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
