673da998813460d6f0a3a07837e3df7b166c77a9
Mariya1255 <162633244+Mariya1255@users.noreply.github.com>
2025-12-13 04:24:35 +0500

    'save'

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   url: 'https://Mariya1255.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-      baseUrl: '/humanoid-robotics-book/',
+      baseUrl: '/my-docusaurus-book/',
       
       // GitHub pages deployment config.
       // If you aren't using GitHub pages, you don't need these.
